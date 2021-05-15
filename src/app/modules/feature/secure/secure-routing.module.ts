d44f7062631c28bb39c159728a44df483217aad8
Saivivek Rasala <viveksai26@gmail.com>
2021-05-15 16:32:55 +0530

    Dashboard added

--- src/app/modules/feature/secure/secure-routing.module.ts
+++ src/app/modules/feature/secure/secure-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then((module) => module.DashboardModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
